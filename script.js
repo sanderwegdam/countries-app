24a67ac6215a3b0495b60887778a82d80c77cf9f
nouser <nouser>
2023-10-31 14:17:16 +0100

    update

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const regionDetails = document.getElementById('regionDetails');
-    const regionLinks = document.querySelectorAll('.region-link');
-  
-    // Voeg een click-eventlistener toe aan de regio's
-    regionLinks.forEach(function (regionLink) {
-      regionLink.addEventListener('click', function () {
-        // Haal gegevens van de geklikte regio op
-        const regionName = regionLink.textContent;
-        fetchRegionData(regionName);
-      });
-    });
-  
-    // Functie om gegevens van een specifieke regio op te halen
-    function fetchRegionData(regionName) {
-      fetch('data.json') // Vervang 'data.json' door de daadwerkelijke URL van je JSON-bestand
-        .then(response => response.json())
-        .then(data => {
-          const region = data.find(item => item.name === regionName);
-  
-          if (region) {
-            displayRegionDetails(region);
-          } else {
-            console.error('Regio niet gevonden.');
-          }
-        })
-        .catch(error => {
-          console.error('Fout bij het ophalen van gegevens: ' + error);
-        });
+const countriesContainerDOM = document.querySelector("#countries-container");
+const url = "data.json";
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
     }
-  
-    // Functie om regio-details weer te geven
-    function displayRegionDetails(region) {
-      regionDetails.innerHTML = ''; // Leeg de regio-details div
-      const regionTitle = document.createElement('h2');
-      regionTitle.textContent = region.name;
-      regionDetails.appendChild(regionTitle);
-      // Voeg andere eigenschappen van de regio toe aan regionDetails
-      // Voorbeeld: topLevelDomain, alpha2Code, enz.
-    }
-  });
+
+    const data = await response.json();
+    displayCountries(data);
+  } catch (error) {
+    console.error(error); // Toon de fout in de console
+  }
+};
+
+fetchData();
+
+const displayCountries = (data) => {
+  const countries = data
+    .map(({ flag, name, population, region, capital, alpha3Code: code }) => {
+      return `<a href="country.html?code=${code}" class="card">
+        <img src="${flag}" alt="${name} flag" width="320" class="img" />
+        <footer>
+          <h2>${name}</h2>
+          <p><span>Population:</span> ${population}</p>
+          <p><span>Region:</span> ${region}</p>
+          <p><span>Capital:</span> ${capital}</p>
+        </footer>
+      </a>`;
+    })
+    .join("");
+  countriesContainerDOM.innerHTML = countries;
+};
--- toggle.js
+++ toggle.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
-    const regionList = document.getElementById('regionList');
+    const toggleButton = document.getElementById('toggle-button');
+    const regionList = document.getElementById('region-list');
 
     toggleButton.addEventListener('click', function (e) {
         if (regionList.classList.contains('region-open')) {
@@ -10,3 +10,27 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+const dropdownBtn = document.querySelector("#toggle-button");
+
+const dropdownFiltering = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("region-link")) {
+    dropdownBtn.dataset.selected = e.target.dataset.id;
+
+    if (e.target.dataset.id === "All") {
+      dropdownBtn.textContent = "Filter by Region";
+    } else {
+      dropdownBtn.textContent = `${e.target.textContent}`;
+    }
+  }
+};
+
+const regionLinks = document.querySelectorAll(".region-link");
+
+regionLinks.forEach(link => {
+  link.addEventListener("click", dropdownFiltering);
+});
+
+// Om de initiële tekst in te stellen:
+dropdownBtn.textContent = "Filter by Region";
