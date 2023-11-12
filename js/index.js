5d9ade199b121949f3e1fa842222d1e6a57c6387
Sander Wegdam <sanderwegdam@gmail.com>
2023-11-12 15:58:50 +0100

    update

--- js/index.js
+++ js/index.js
@@ -10,23 +10,36 @@ async function fetchDataAndDisplay() {
 
     const dropdownBtn = document.querySelector("#toggle-button");
     const searchInput = document.querySelector("#search-input");
-
+    const regionList = document.getElementById('region-list');
+    const filterRegion = document.querySelector('.filter-region');
+    
     const dropdownFiltering = (e) => {
       e.preventDefault();
-      if (e.target.classList.contains("region-link")) {
-        dropdownBtn.dataset.selected = e.target.dataset.id;
-
-        if (e.target.dataset.id === "All") {
-          dropdownBtn.textContent = "Filter by Region";
+      const isRegionLink = e.target.classList.contains("region-link");
+    
+      if (isRegionLink) {
+        const newSelectedRegion = e.target.dataset.id;
+        dropdownBtn.dataset.selected = newSelectedRegion;
+    
+        if (newSelectedRegion === "All") {
+          filterRegion.textContent = "Region: All";
         } else {
-          dropdownBtn.textContent = `${e.target.textContent}`;
+          filterRegion.textContent = `Region: ${e.target.textContent}`; // Voeg een pijl toe
         }
+        closeDropdownMenu();
         filterAndDisplayCountries();
       }
     };
 
+    const closeDropdownMenu = () => {
+      // Voeg hier je logica toe om het dropdown-menu te sluiten
+      // Bijvoorbeeld, als je een CSS-klasse gebruikt om het menu te verbergen:
+      // dropdownMenu.classList.add("hidden");
+      regionList.classList.remove("region-open");
+    };
+    
     const regionLinks = document.querySelectorAll(".region-link");
-
+    
     regionLinks.forEach(link => {
       link.addEventListener("click", dropdownFiltering);
     });
@@ -61,7 +74,7 @@ async function fetchDataAndDisplay() {
 
     filterAndDisplayCountries();
 
-    dropdownBtn.textContent = "Filter by Region";
+    // dropdownBtn.textContent = "Filter by Region";
   } catch (error) {
     console.log("Error in fetchDataAndDisplay:", error);
   }
